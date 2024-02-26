0911f6692ffa5b63e1dea324f9413d2d6844f25b
Viner108 <andrukovichl@mail.ru>
2024-02-26 12:15:27 +0300

    корректировка после merge

--- src/main/java/tank/TankApplication.java
+++ src/main/java/tank/TankApplication.java
@@ -2,14 +2,18 @@ package tank;
 
 import tank.connection.ClientListenerThread;
 import tank.connection.FullConnection;
+import tank.connection.InputConnection;
+import tank.connection.OutputConnection;
 
 import java.io.*;
 
 public class TankApplication {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ClientListenerThread connection = new ClientListenerThread();
-        connection.start();
+        InputConnection inputConnection = new InputConnection();
+        OutputConnection outputConnection =new OutputConnection();
+        inputConnection.start();
+        outputConnection.start();
 
 
     }
